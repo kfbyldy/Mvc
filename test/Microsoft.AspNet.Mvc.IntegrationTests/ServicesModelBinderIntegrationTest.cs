46c99e261ec83ed231bc90c5e49113ba8f73632e
Doug Bunting <dougbu@microsoft.com>
2015-10-11 13:13:14 -0700

    Test `[FromServices]` with service that is not available - also test `[FromServices]` for a defined type without a `BinderModelName`

--- test/Microsoft.AspNet.Mvc.IntegrationTests/ServicesModelBinderIntegrationTest.cs
+++ test/Microsoft.AspNet.Mvc.IntegrationTests/ServicesModelBinderIntegrationTest.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc.Abstractions;
 using Microsoft.AspNet.Mvc.Formatters;
@@ -131,6 +132,69 @@ namespace Microsoft.AspNet.Mvc.IntegrationTests
             // ModelState
             Assert.True(modelState.IsValid);
             Assert.Empty(modelState.Keys);
+        }
+
+        [Fact]
+        public async Task BindParameterFromService_NoPrefix_GetsBound()
+        {
+            // Arrange
+            var argumentBinder = ModelBindingTestHelper.GetArgumentBinder();
+            var parameter = new ParameterDescriptor()
+            {
+                Name = "ControllerProperty",
+                BindingInfo = new BindingInfo()
+                {
+                    BindingSource = BindingSource.Services
+                },
+
+                // Using a service type already in defaults.
+                ParameterType = typeof(JsonOutputFormatter)
+            };
+
+            var operationContext = ModelBindingTestHelper.GetOperationBindingContext();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            var modelBindingResult = await argumentBinder.BindModelAsync(parameter, modelState, operationContext);
+
+            // Assert
+
+            // ModelBindingResult
+            Assert.True(modelBindingResult.IsModelSet);
+
+            // Model
+            var outputFormatter = Assert.IsType<JsonOutputFormatter>(modelBindingResult.Model);
+            Assert.NotNull(outputFormatter);
+
+            // ModelState
+            Assert.True(modelState.IsValid);
+            Assert.Empty(modelState.Keys);
+        }
+
+        [Fact]
+        public async Task BindParameterFromService_NoService_Throws()
+        {
+            // Arrange
+            var argumentBinder = ModelBindingTestHelper.GetArgumentBinder();
+            var parameter = new ParameterDescriptor()
+            {
+                Name = "ControllerProperty",
+                BindingInfo = new BindingInfo()
+                {
+                    BindingSource = BindingSource.Services
+                },
+
+                // Using a service type already in defaults.
+                ParameterType = typeof(IActionResult),
+            };
+
+            var operationContext = ModelBindingTestHelper.GetOperationBindingContext();
+            var modelState = new ModelStateDictionary();
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(
+                () => argumentBinder.BindModelAsync(parameter, modelState, operationContext));
+            Assert.Contains(typeof(IActionResult).FullName, exception.Message);
         }
     }
 }
